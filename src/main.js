416025f1c13ca01261ab430dda9446e6c4c1c757
Son Do <dominhson580@gmail.com>
2025-05-08 23:13:54 +0700

    Define and test ShipFactory

--- src/main.js
+++ src/main.js
@@ -1,3 +1,42 @@
-export function sum(a, b) {
-  return a + b;
+export const ERROR_MESSAGE = {
+  INVALID_SHIP_LENGTH: "Invalid ship length",
+};
+
+const SHIP_LENGTH_MAP = {
+  carrier: 5,
+  battleShip: 4,
+  destroyer: 3,
+  submarine: 3,
+  patrolBoat: 2,
+};
+
+const validShipLengthSet = new Set(Object.values(SHIP_LENGTH_MAP));
+
+export function ShipFactory(length) {
+  if (!validShipLengthSet.has(length)) {
+    throw new Error(ERROR_MESSAGE.INVALID_SHIP_LENGTH);
+  }
+
+  let hit_count = 0;
+  let sunk = false;
+
+  function getHit() {
+    if (hit_count == length) {
+      return;
+    }
+
+    hit_count++;
+    if (hit_count == length) {
+      sunk = true;
+    }
+  }
+
+  function isSunk() {
+    return sunk;
+  }
+
+  return {
+    getHit,
+    isSunk,
+  };
 }
--- src/main.test.js
+++ src/main.test.js
@@ -1,5 +1,28 @@
-import { sum } from "./main";
+import { ERROR_MESSAGE, ShipFactory } from "./main";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("ShipFactory", () => {
+  it("should throw error for invalid ship length", () => {
+    expect(() => ShipFactory(1)).toThrow(ERROR_MESSAGE.INVALID_SHIP_LENGTH);
+    expect(() => ShipFactory(6)).toThrow(ERROR_MESSAGE.INVALID_SHIP_LENGTH);
+    expect(() => ShipFactory(0.5)).toThrow(ERROR_MESSAGE.INVALID_SHIP_LENGTH);
+  });
+
+  it("ship should be sunk when fully hit", () => {
+    const length = 3;
+    const ship = ShipFactory(length);
+
+    // not sunk before fully hit
+    for (let i = 0; i < length - 1; i++) {
+      ship.getHit();
+      expect(ship.isSunk()).toBe(false);
+    }
+
+    // sunk when fully hit
+    ship.getHit();
+    expect(ship.isSunk()).toBe(true);
+
+    // remain sunk after fully hit
+    ship.getHit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
